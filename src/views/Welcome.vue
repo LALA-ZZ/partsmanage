23844369fdf86a4a56aad9ca8e3b8a1d9407ecc6
LALA-ZZ <1525060416@qq.com>
2021-03-08 19:49:08 +0800

    欢迎界面

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,7 +1,7 @@
 
 <!--  -->
 <template>
-  <div>
+  <div class="welcome-contain">
     Welcome!
   </div>
 </template>
@@ -24,4 +24,14 @@ export default {
 
 
 <style lang="scss" scoped>
+.welcome-contain {
+  /*flex 布局*/
+  display: flex;
+  /*实现垂直居中*/
+  align-items: center;
+  /*实现水平居中*/
+  justify-content: center;
+  height: 100%;
+  font-size: 200px;
+}
 </style>
